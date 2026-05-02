d5fcc732aa3b8628c4d0f425ab4503a254587126
Hashwanth <hashwanth107@gmail.com>
2026-05-02 10:12:45 +0530

    Add files via upload

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./index.html", "./script.js", "./script.jsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Segoe UI", "ui-sans-serif", "system-ui", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+};
